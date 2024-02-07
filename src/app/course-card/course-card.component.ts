babfd32a36171afad9d687a683e657e0f957221d
Hmayda <abdoessamadhmayda@gmail.com>
2024-02-07 15:12:04 +0100

    @if + *ngIf + @for + *ngFor + @Input + @Output

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -20,4 +20,10 @@ export class CourseCardComponent {
     this.courseSelected.emit(this.course)
   }
 
+  CardClasses(){
+    if(this.course.category.toLowerCase()=="beginner"){
+      return 'beginner';
+    }
+  }
+
 }
