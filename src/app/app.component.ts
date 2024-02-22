58a5d4520cd877c998c9b054cf4678fe9cffa4fc
Hmayda <abdoessamadhmayda@gmail.com>
2024-02-22 22:08:48 +0100

    just finished learning the built in pipes in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,21 @@
-import {Component} from '@angular/core';
+  import {Component} from '@angular/core';
 
 
 
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent  {
+  @Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+  })
+  export class AppComponent  {
 
+    title="Learning "
 
+    date =new Date()
 
+    price=99.9999599999
 
+    rate=0.34
 
-}
+  }
